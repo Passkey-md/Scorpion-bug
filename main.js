5ad5b80e82190194f10b482723af4049834989b3
Zaibi hacker <mrzaibi65@gmail.com>
2024-07-21 11:18:22 +0500

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "2348108395270"
+let phoneNumber = "923107552846"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
